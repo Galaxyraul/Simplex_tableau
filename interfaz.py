940433db5b1725224fcedcbc57bb9e0a7e0a6ec9
Raul Gomez Tellez <rgt00024@red.ujaen.es>
2024-10-10 14:07:06 +0200

    autogen frames and buttons

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,8 @@
 import customtkinter as ctk
 current = [0]
+tab_list = [
+    "Simplex Tableau","Función Objetivo","Restricciones","Tableau","Historial"
+]
 def change_frame(remove,render):
     print(f"cambiando pestañas {remove,render}")
     frames[remove].pack_forget()
@@ -21,22 +24,19 @@ app.geometry("1000x800")
 app.title("Interfaz Principal")
 
 # Crear el frame izquierdo (menú lateral)
-sidebar = ctk.CTkFrame(master=app, width=150,border_width=2)
+sidebar = ctk.CTkFrame(master=app, width=300,border_width=2)
 sidebar.pack(side="left", fill="y")
 
 name = ctk.CTkFrame(sidebar,fg_color="red",height=100)
-name.pack(side="top",fill="x",padx=2)
-label_name = ctk.CTkLabel(name,text="Simplex Tableau")
-label_name.pack(expand=True)
+name.pack(side="top",fill="x",padx=2,pady = 20)
 
 options = ctk.CTkFrame(sidebar,height=600,fg_color="transparent")
 options.pack(side="bottom",pady=50,fill="x",padx=2)
 
 buttons = []
-
-for i in range(5):
-    button = ctk.CTkButton(options, text=f"Tab {i + 1}", command=lambda i=i : change_frame(current[0],i),fg_color="transparent",corner_radius=0)
-    button.pack(pady=10, fill="x",padx=2)
+for i in range(len(tab_list)):
+    button = ctk.CTkButton(options if i != 0 else name, text=tab_list[i], command=lambda i=i : change_frame(current[0],i),fg_color="transparent",corner_radius=0)
+    button.pack(pady=10, fill="x")
     buttons.append(button)
 # Crear un contenedor para las pantallas
 main = ctk.CTkFrame(master=app)
@@ -45,15 +45,16 @@ main.pack(side="right", fill="both", expand=True)
 
 frames = []
 
-for i in range(5):
+for i in range(len(tab_list)):
     frame = ctk.CTkFrame(main) 
     frame.pack(fill="both", expand=True)
-    label = ctk.CTkLabel(frame, text=f"This is the {i + 1} tab")
+    label = ctk.CTkLabel(frame, text=f"This is the {tab_list[i]} tab")
     label.pack(pady=10)
     frame.pack_forget()
     frames.append(frame)
 
-frames[current[0]].pack(fill="both", expand=True)
 
+frames[current[0]].pack(fill="both", expand=True)
+buttons[current[0]].configure(fg_color="transparent")
 app.mainloop()
 
